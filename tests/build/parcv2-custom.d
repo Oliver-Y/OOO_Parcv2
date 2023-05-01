3e62857a6fdcc908e85af1808cb69cdfa470327a
Ye Oliver <omy3@zoo.cs.yale.edu>
2023-04-30 20:49:28 -0400

    hi

---
+++ tests/build/parcv2-custom.d
@@ -0,0 +1,3 @@
+parcv2-custom: ../parcv2/parcv2-custom.S ../parcv2/parc-macros.h
+
+../parcv2/parc-macros.h:
---
+++ tests/build/parcv2-special.d
@@ -0,0 +1,3 @@
+parcv2-special: ../parcv2/parcv2-special.S ../parcv2/parc-macros.h
+
+../parcv2/parc-macros.h:
